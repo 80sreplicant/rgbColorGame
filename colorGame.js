0f2955a26ff937621bdb6bf2b90d80016df53db9
unknown <jdnyc84@gmail.com>
2020-06-01 19:01:00 -0400

    js functionality

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,79 @@
-const color = [
-  'rgb(255, 0, 0)',
-  'rgb(255, 255, 0)',
-  'rgb(0, 255, 0)',
-  'rgb(0, 255, 255)',
-  'rgb(0, 0, 255)',
-  'rgb(255, 0, 255)',
-];
+let colors = generateRandomColors(6);
 
 const squares = document.querySelectorAll('.square');
-let pickedColor = color[3];
+let pickedColor = pickColor();
+let colorDisplay = document.querySelector('#colorDisplay');
+let messageDisplay = document.querySelector('#message');
+let h1 = document.querySelector('h1');
+const resetButton = document.querySelector('#reset');
+
+resetButton.addEventListener('click', function () {
+  // generate all new colors
+  colors = generateRandomColors(6);
+  // pick a random new color from array
+  pickedColor = pickColor();
+  //   change colorDisplay to make pickedColor
+  colorDisplay.textContent = pickedColor;
+  // change the colors of the squares
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = '#232323';
+});
+
+colorDisplay.textContent = pickedColor;
 
 for (let i = 0; i < squares.length; i++) {
-  squares[i].style.backgroundColor = color[i];
+  // add initial colors to squares
+  squares[i].style.backgroundColor = colors[i];
+  // add click listeners to squares
+  squares[i].addEventListener('click', function () {
+    // grab color of picked square
+    let clickedColor = this.style.backgroundColor;
+    // compare color to pickedColor
+    if (clickedColor === pickedColor) {
+      messageDisplay.textContent = 'Correct!!!';
+      resetButton.textContent = 'Play Again?';
+      changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
+    } else {
+      this.style.backgroundColor = '#232323';
+      messageDisplay.textContent = 'Try Again';
+    }
+  });
+}
+
+function changeColors(color) {
+  // loop through all the squares
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+  // change each color to match given color
+}
+
+function pickColor() {
+  let random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num) {
+  // make an array
+  const arr = [];
+  // repeat num times
+  for (let i = 0; i < num; i++) {
+    // get random color and push into arr
+    arr.push(randomColor());
+  }
+  // return that array
+  return arr;
+}
+
+function randomColor() {
+  // pick a red from 0 to 255
+  let r = Math.floor(Math.random() * 256);
+  // pick a green from 0 to 255
+  let b = Math.floor(Math.random() * 256);
+  // pick a blue from 0 to 255
+  let g = Math.floor(Math.random() * 256);
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
